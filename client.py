f8a0a13bfa4e4c862da6b7c07c86e9b2b31b117d
Peter Hayes <peter.k.hayes@gmail.com>
2015-03-21 20:55:10 -0700

    updates

--- client.py
+++ client.py
@@ -1,16 +1,17 @@
 import random
 import socket
 import time
+# import pillow
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 6666
 
 while True:
     MESSAGE = "%s %s %s" % (
-        random.randint(0, 100),
-        random.randint(0, 100),
+        random.randint(0, 99),
+        random.randint(0, 99),
         random.choice(['rebeccapurple', 'black', 'blue', 'red', 'green'])
     )
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto(MESSAGE, ('127.0.0.1', 6666))
-    time.sleep(1)
+    time.sleep(0.01)
--- server.js
+++ server.js
@@ -2,28 +2,31 @@ var express = require('express');
 
 var app = express();
 var img_data = [];
-var server = require('http').Server(app)
+var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-for(var i = 0; i < 100; i++) {
+var SIZE = 100;
+var UPDATE = 100;
+
+for(var i = 0; i < SIZE; i++) {
     img_data.push([]);
-    for ( var j = 0; j < 100; j++) {
+    for ( var j = 0; j < SIZE; j++) {
         img_data[i].push(null);
     }
 }
 
-app.get("/", express.static('public'))
+app.get("/", express.static('public'));
 
-server.listen(7777)
+server.listen(7777);
 var socket;
 
 var send_data = function() {
-    socket.emit('IMG', img_data)
-  }
+    socket.emit('IMG', img_data);
+};
 
 io.on('connection', function (mysocket) {
   socket = mysocket;
-  setInterval(send_data, 100);
+  setInterval(send_data, UPDATE);
 });
 
 var dgram = require('dgram');
@@ -32,19 +35,19 @@ var server = dgram.createSocket('udp4');
 server.on('listening', function () {
     var address = server.address();
     console.log('UDP Server listening on ' + address.address + ":" + address.port);
+    console.log('HTTP Server listening on 0.0.0.0:7777');
 });
 
 server.on('message', function (message, remote) {
     parts = String(message).split(' ');
 
-    x_coord = parseInt(parts[0]);
-    y_coord = parseInt(parts[1]);
+    x_coord = Number(parts[0]);
+    y_coord = Number(parts[1]);
 
-    console.log(remote.address + ':' + remote.port +' - ' + x_coord + ' '  + y_coord);
+    if (!(x_coord % 1) && x_coord < 100 && x_coord >= 0 && !(y_coord % 1) && y_coord < 100 && y_coord >= 0) {
+        img_data[y_coord][x_coord] = parts[2].replace(/\s/g, '');
+    }
 
-    img_data[y_coord][x_coord] = parts[2].replace(/\s/g, '');
-
-    console.log(img_data);
 });
 
 server.bind(6666, '0.0.0.0');
